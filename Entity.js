e6b281106f4161879a137c83dcaa3c2e5082f545
metamodellum <metamodellum@gmail.com>
2025-02-22 17:07:00 +0000

    Add Entity and Sprite classes for in-game objects and their images

---
+++ Entity.js
@@ -0,0 +1,20 @@
+/**
+ * Defines a character, item, etc. Objects that live on the map.
+ */
+class Entity {
+    constructor(config){
+        //co-ordinates 
+        this.x = config.x || 0 //sets x, defualts to 0 if undefined
+        this.y = config.y || 0 //sets y, defualts to 0 if undefined
+        console.log("Creating Entity with x:", this.x, "y:", this.y);
+
+
+        // creates new instance of Sprite for the new instance of Entity 
+        this.sprite = new Sprite({
+            entity: this, // links the sprite to this entity 
+            src: config.src || "./assets/images/sprites/characters/human.png"
+        });
+
+        console.log("Entity sprite:", this.sprite);
+
+}}
---
+++ Sprite.js
@@ -0,0 +1,49 @@
+/**
+ * Represents the images of entities, handles image rendering.
+ */
+class Sprite {
+    constructor(config) {
+        this.image = new Image(); // creates new instance of image 
+        this.image.src = config.src; // sets the source of the image 
+        this.isLoaded = false;
+
+        // waits for the image to load (image.src), then sets isLoaded = true 
+        this.image.onload = () => {
+            this.isLoaded = true;
+        };
+
+        this.image.onerror = (err) => {
+            console.error("Error loading sprite image:", err);
+        };
+
+        // Animation setup
+        this.animation = config.animation || {
+            idleDown: [[0, 0]]
+        };
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
+
+        // Reference to the entity object this instance of sprite belongs to
+        this.entity = config.entity; 
+    }
+
+    /**
+     * Method belonging to the instance of sprite 
+     * @param {*} ctx 
+     * @returns 
+     */
+    draw(ctx) {
+        if (!this.isLoaded) return;  // Prevent drawing if image isn't loaded
+
+        const [x, y] = [this.entity.x * 16, this.entity.y * 16] // coverts grid coordinates to the pixel values
+
+        //Actually draws the sprite on the canvas
+        ctx.drawImage(
+            this.image,
+            0, 0,
+            16, 16,
+            x, y,
+            16, 16
+        );
+    }
+}
