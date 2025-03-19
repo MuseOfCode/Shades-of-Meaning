ecb91edee1037b3c9347f7a9db6b23cd4f535fff
Ken'Terria Reaves <kenterria.z.reaves@gmail.com>
2025-03-19 14:33:12 +0000

    feat: add sprite animation handling with different states and frame progression

--- Sprite.js
+++ Sprite.js
@@ -2,48 +2,98 @@
  * Represents the images of entities, handles image rendering.
  */
 class Sprite {
-    constructor(config) {
-        this.image = new Image(); // creates new instance of image 
-        this.image.src = config.src; // sets the source of the image 
-        this.isLoaded = false;
+  constructor(config) {
+    this.image = new Image(); // creates new instance of image
+    this.image.src = config.src; // sets the source of the image
+    this.isLoaded = false;
 
-        // waits for the image to load (image.src), then sets isLoaded = true 
-        this.image.onload = () => {
-            this.isLoaded = true;
-        };
+    // waits for the image to load (image.src), then sets isLoaded = true
+    this.image.onload = () => {
+      this.isLoaded = true;
+    };
 
-        this.image.onerror = (err) => {
-            console.error("Error loading sprite image:", err);
-        };
+    this.image.onerror = (err) => {
+      console.error("Error loading sprite image:", err);
+    };
 
-        // Animation setup
-        this.animation = config.animation || {
-            idleDown: [[0, 0]]
-        };
-        this.currentAnimation = config.currentAnimation || "idleDown";
-        this.currentAnimationFrame = 0;
+    // Animation setup
+    this.animations = config.animations || {
+      "idle-down": [[0, 0]],
+      "idle-right": [[0, 1]],
+      "idle-up": [[0, 2]],
+      "idle-left": [[0, 3]],
+      "walk-down": [
+        [1, 0],
+        [0, 0],
+        [3, 0],
+        [2, 0],
+      ],
+      "walk-right": [
+        [1, 1],
+        [0, 1],
+        [3, 1],
+        [2, 1],
+      ],
+      "walk-up": [
+        [1, 2],
+        [0, 2],
+        [3, 2],
+        [2, 2],
+      ],
+      "walk-left": [
+        [1, 3],
+        [0, 3],
+        [3, 3],
+        [2, 3],
+      ],
+    };
+    this.activeAnimationKey = config.currentAnimation || "idle-down";
+    this.animationFrameIndex = 0;
 
-        // Reference to the entity object this instance of sprite belongs to
-        this.entity = config.entity; 
+    this.animationFrameLimit = config.animationFrameLimit || 8;
+    this.animationFrameProgress = this.animationFrameLimit;
+
+    // Link the sprite to the associated game object.
+    this.entity = config.entity;
+  }
+
+  get frame() {
+    return this.animations[this.activeAnimationKey][this.animationFrameIndex];
+  }
+
+  setAnimation(animationKey) {
+    if (this.activeAnimationKey !== animationKey) {
+      this.activeAnimationKey = animationKey;
+      this.animationFrameIndex = 0;
+      this.animationFrameProgress = this.animationFrameLimit;
     }
+  }
 
-    /**
-     * Method belonging to the instance of sprite 
-     * @param {*} ctx 
-     * @returns 
-     */
-    draw(ctx) {
-        if (!this.isLoaded) return;  // Prevent drawing if image isn't loaded
-
-        const [x, y] = [this.entity.x, this.entity.y] // coverts grid coordinates to the pixel values
-
-        //Actually draws the sprite on the canvas
-        ctx.drawImage(
-            this.image,
-            0, 0,
-            16, 16,
-            x, y,
-            16, 16
-        );
+  updateAnimationProgress() {
+    if (this.animationFrameProgress > 0) {
+      this.animationFrameProgress -= 1;
+      return;
     }
+    this.animationFrameProgress = this.animationFrameLimit;
+    this.animationFrameIndex += 1;
+
+    if (this.frame === undefined) {
+      this.animationFrameIndex = 0;
+    }
+  }
+  /**
+   * Method belonging to the instance of sprite
+   * @param {*} ctx
+   * @returns
+   */
+  draw(ctx) {
+    if (!this.isLoaded) return; // Prevent drawing if image isn't loaded
+
+    const [x, y] = [this.entity.x, this.entity.y];
+    const [frameX, frameY] = this.frame;
+
+    // Actually draws the sprite on the canvas
+    ctx.drawImage(this.image, frameX * 16, frameY * 16, 16, 16, x, y, 16, 16);
+    this.updateAnimationProgress();
+  }
 }
--- WorldMap.js
+++ WorldMap.js
@@ -1,75 +1,72 @@
 class WorldMap {
-    constructor(config){
-        this.worldState = config.worldState //refrencing worldState obj
+  constructor(config) {
+    this.worldState = config.worldState; //refrencing worldState obj
 
-        //base layer of map
-        this.baseImg = new Image()
-        this.baseImg.src = config.baseSrc
+    //base layer of map
+    this.baseImg = new Image();
+    this.baseImg.src = config.baseSrc;
 
-        //top layer of map
-        this.topImg = new Image()
-        this.topImg.src = config.topSrc
-    }
+    //top layer of map
+    this.topImg = new Image();
+    this.topImg.src = config.topSrc;
+  }
 
-    drawBaseImg(ctx){
-        ctx.drawImage(this.baseImg, 0, 0)
-    }
+  drawBaseImg(ctx) {
+    ctx.drawImage(this.baseImg, 0, 0);
+  }
 
-    drawTopImg(ctx){
-        ctx.drawImage(this.topImg, 0, 0)
-    }
+  drawTopImg(ctx) {
+    ctx.drawImage(this.topImg, 0, 0);
+  }
 }
 
-//object container all the maps, can be used across all files 
+//object container all the maps, can be used across all files
 window.WorldMaps = {
-    Bedroom: {
-        baseSrc : "./assets/images/maps/base/bedroom.png",
-        topSrc : "./assets/images/maps/top/bedroom.png",
-        entities : {
-            humanAvatar : new Actor({
-                x : 5,
-                y : 5,
-            }),
-            canineAvatar : new Actor({
-                x : 5,
-                y : 6,
-                src : "./assets/images/sprites/characters/canine.png"
-            })
-        }
-
+  Bedroom: {
+    baseSrc: "./assets/images/maps/base/bedroom.png",
+    topSrc: "./assets/images/maps/top/bedroom.png",
+    entities: {
+      humanAvatar: new Actor({
+        x: 5,
+        y: 5,
+      }),
+      canineAvatar: new Actor({
+        x: 5,
+        y: 6,
+        src: "./assets/images/sprites/characters/canine.png",
+      }),
     },
+  },
 
-    Lobby : {
-        baseSrc : "./assets/images/maps/base/lobby.png",
-        topSrc : "./assets/images/maps/top/lobby.png",
-        entities : {
-            humanAvatar : new Actor({
-                x: utils.gridify(3),
-                y: utils.gridify(5),
-            }),
-            canineAvatar : new Actor({
-                x: utils.gridify(4),
-                y: utils.gridify(5),
-                src : "./assets/images/sprites/characters/canine.png"
-            })
-        }
-
+  Lobby: {
+    baseSrc: "./assets/images/maps/base/lobby.png",
+    topSrc: "./assets/images/maps/top/lobby.png",
+    entities: {
+      humanAvatar: new Actor({
+        x: utils.gridify(3),
+        y: utils.gridify(5),
+      }),
+      canineAvatar: new Actor({
+        x: utils.gridify(4),
+        y: utils.gridify(5),
+        src: "./assets/images/sprites/characters/canine.png",
+      }),
     },
+  },
 
-    Village : {
-        baseSrc : "./assets/images/maps/base/village.png",
-        entities : {
-            humanAvatar : new Actor({
-                // isPlayerControlled : true,
-                x: utils.gridify(7),
-                y: utils.gridify(5),
-            }),
-            canineAvatar : new Actor({
-                x: utils.gridify(6),
-                y: utils.gridify(5),
-                src : "./assets/images/sprites/characters/canine.png"
-            })
-        }
-
-    }
-}
+  Village: {
+    baseSrc: "./assets/images/maps/base/village.png",
+    entities: {
+      humanAvatar: new Actor({
+        // isPlayerControlled : true,
+        x: utils.gridify(7),
+        y: utils.gridify(5),
+      }),
+      canineAvatar: new Actor({
+        x: utils.gridify(6),
+        y: utils.gridify(5),
+        src: "./assets/images/sprites/characters/canine.png",
+      }),
+    },
+  },
+};
